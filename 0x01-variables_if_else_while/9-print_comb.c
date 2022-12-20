faccf8e59fa3445b752387382fd560e024c47470
Bereket Ferde <bereket547@example.com>
2022-12-20 09:03:18 -0800

    print nested putchar

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'print numbers separated by , and $ sign'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int a;
+
+for (a = 0 ; a < 10 ; a++)
+{
+putchar(a % 10 + '0');
+if (a == 9)
+{
+putchar('$');
+break;
+}
+putchar(',');
+putchar(' ');
+
+return (0);
+}
