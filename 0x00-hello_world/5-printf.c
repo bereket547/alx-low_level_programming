625df7469b98b3c08927f82b885ef356093400e0
Bereket Ferde <bereket547@example.com>
2022-12-20 00:46:58 -0800

    printf command

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,27 +1,16 @@
-#include<stdio.h>                                                                                 
+#include<stdio.h>
 
-                                                                                                  
+/**
+ * main - Entry point
+ * 
+ * Description: 'printing sentences using printf'
+ *
+ * Return: Always 0 (Success)
+ */
 
-/**                                                                                               
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
 
- * main -Entry point                                                                              
-
- *                                                                                                
-
- * Description: 'printing a sentence using printf'                                                
-
- *                                                                                                
-
- * Return: Always 0 (Success)                                                                     
-
- */                                                                                               
-
-                                                                                                  
-
-int main(void)                                                                                    
-
-{                  
-	printf("with proper grammar, but the outcome is a piece of art,");
-
-	return (0);
+return (0);
 }
