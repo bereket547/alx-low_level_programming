75199aae9d163662b918f9e94919ee0acdaac899
Bereket Ferde <bereket547@example.com>
2022-12-19 23:41:43 -0800

    hello c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,7 @@
 #include<stdio.h>
-int main()
+int main(void)
 {
-char Mystr[] = "Programming is like building a multilingual puzzle\n";
-
-puts(Mystr);
-
-return 0;
+	char Mystr[] = "\"Programming is like building a multilingual puzzle\n";
+	puts(Mystr);
+	return 0;
 }
