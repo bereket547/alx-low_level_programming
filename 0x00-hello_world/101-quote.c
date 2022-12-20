6ad7f6c6eb79a83f4520ca323e0b4fe6107a5d3d
Bereket Ferde <bereket547@example.com>
2022-12-20 03:00:19 -0800

    simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program prints a series of string'
+ *
+ * Return: Always 1(Success)
+ */
+
+int main(void)
+{
+  char x = '-';
+  char name[] = "Dora Korpar";
+
+  printf("and that piece of art is useful\" %x %name, 2015%x10%x19\n");
+  return (1);
+}
