b91789d0bf13fa47d0ae6d8d3420ef2be6027535
Bereket Ferde <bereket547@example.com>
2022-12-20 07:05:05 -0800

    for loop

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,7 +2,7 @@
 #include<stdlib.h>
 #include<time.h>
 
-/** 
+/**
  * main - Entry point
  *
  * Description: 'last digit checking'
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'printing lowercase a-z'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char ch;
+
+for (ch = 'a' ; ch <= 'z' ; ch++)
+putchar(ch);
+
+return (0);
+}
