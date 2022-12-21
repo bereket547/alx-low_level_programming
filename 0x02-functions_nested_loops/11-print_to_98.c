4427da0d818a1046848ba6ffa971c0a562134b84
Bereket Ferde <bereket547@example.com>
2022-12-21 06:22:26 -0800

    98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,20 +11,24 @@
 
 void print_to_98(int n)
 {
-if (n < 98)
+if (n >= 98)
 {
-for ( ; n <= 98 ; n++)
+for ( ; n >= 98 ; n--)
 {
 printf("%d", n);
+if (n == 98);
+break;
 printf(",");
 printf(" ");
 }
 }
 else
 {
-for ( ; n >= 98 ; n--)
+for ( ; n <= 98 ; n++)
 {
 printf("%d", n);
+if (n == 98)
+break;
 _putchar(',');
 _putchar(' ');
 }
