75107043f589e53792f855a88b9c08d41a1adb72
Bereket Ferde <bereket547@example.com>
2022-12-21 04:31:19 -0800

    24 hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Entry point
+ * @n: input number
+ * Description: 'print digit of a 24 hour'
+ *
+ * Return: Always 0 (Success)
+ */
+
+void jack_bauer(void n)
+{
+int a, b, c, d;
+for (a = 0 ; a < 3 ; a++)
+{
+for (b = 0 ; b < 10 ; b++)
+{
+for (c = 0 ; c < 10 ; c++)
+{
+for (d = 0 ; d < 10 ; d++)
+{
+_putchar(a + '0');
+_putchar(b + '0');
+_putchar(':');
+_putchar(c + '0');
+_putchar(d + '0');
+_putchar('\n');
+}
+}
+}
+}
+}
