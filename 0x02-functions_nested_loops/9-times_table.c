ac5c4103930ec5ba47143a6cdeeb3ad65b3c2509
Bereket Ferde <bereket547@example.com>
2022-12-21 05:06:52 -0800

    time table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,11 +15,20 @@ for (a = 0 ; a < 10 ; a++)
 for (b = 0 ; b < 10 ; b++)
 {
 c = a * b;
+if (c < 10)
+{
+_putchar(c + '0');
+_putchar(',');
+_putchar(' ');
+}
+else
+{
 _putchar((c / 10) + '0');
 _putchar((c % 10) + '0');
 _putchar(',');
 _putchar(' ');
 }
 _putchar('\n');
+}
 }
 }
