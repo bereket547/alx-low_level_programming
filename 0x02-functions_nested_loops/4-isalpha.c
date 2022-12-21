861e5845dee344d99d658195590f1fef65fc031b
Bereket Ferde <bereket547@example.com>
2022-12-21 02:52:47 -0800

    alphabetic character

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include<ctype.h>
+#include "main.h"
+
+/**
+ * _isalpha - Entry point
+ * @c: single letter input
+ * Description: 'print 1 if it is letter else 0'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _isalpha(int c)
+{
+int d;
+d = isalpha(c);
+if (d == 1)
+return (1);
+else
+return (0);
+_putchar('\n');
+
+}
