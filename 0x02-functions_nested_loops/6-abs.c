2bd1b323a2c3e320d1dc29e9b51e0594be040851
Bereket Ferde <bereket547@example.com>
2022-12-21 03:34:56 -0800

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _abs - Entry point
+ *
+ * Description: 'print + if it is positive, - for negative else 0'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _abs(int)
+{
+int n;
+if (n < 0)
+{
+n = n * -1;
+}
+else if (n == 0 || n > 0)
+n = n;
+return (0);
+
+}
