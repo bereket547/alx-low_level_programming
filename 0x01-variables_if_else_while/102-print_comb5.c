de96161c29dc46072f26c51c24ba3eeb3bfaec6c
Bereket Ferde <bereket547@example.com>
2022-12-20 21:49:34 -0800

    four for loops

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'print numbers separated by , and $ sign'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int a, b, d, e;
+
+for (a = 0 ; a < 10 ; a++)
+{
+for (b = 0 ; b < 10 ; b++)
+{
+for (d = 0 ; d < 10 ; d++)
+{
+for (e = 0 ; e < 10 ; e++)
+{
+if ((a * e) == (b * d))
+e = e + 1;
+putchar(a % 10 + '0');
+putchar(b % 10 + '0');
+putchar(' ');
+putchar(d % 10 + '0');
+putchar(e % 10 + '0');
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+
+putchar('\n');
+return (0);
+}
