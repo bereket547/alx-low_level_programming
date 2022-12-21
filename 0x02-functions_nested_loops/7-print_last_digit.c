8d834afeedba4f09246e72ceba2f490f247f56b3
Bereket Ferde <bereket547@example.com>
2022-12-21 03:50:45 -0800

    print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Entry point
+ * @n: input number
+ * Description: 'print digit of any integer'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int print_last_digit(int n)
+{
+int z = n % 10;
+return (z);
+}
+_putchar(' ');
+
+return (0);
+
+}
