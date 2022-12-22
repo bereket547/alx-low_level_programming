6bcc5c4dd805dc3c792c924e281668999a524b86
Bereket Ferde <bereket547@example.com>
2022-12-22 09:36:18 -0800

    numbers

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,12 +17,13 @@ for (a = 1 ; a <= 100 ; a++)
 {
 if (((a % 3) == 0) && ((a % 5) == 0))
 printf("FizzBuzz");
-else if (((a % 3) == 0) && ((a % 5) != 0))
+else if ((a % 3) == 0)
 printf("Fizz");
-else if (((a % 5) == 0) && ((a % 3) != 0))
+else if ((a % 5) == 0)
 printf("Buzz");
 else
 printf("%d", a);
+if (a != 100)
 printf(" ");
 }
 printf("\n");
