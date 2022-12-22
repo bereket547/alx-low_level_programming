4122cdf4bed82047e812c57b7ca07309ef30e20f
Bereket Ferde <bereket547@example.com>
2022-12-22 07:36:24 -0800

    uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,25 @@
+#include"main.h"
+
+/**
+ * _isupper - tell the letter if it is upper case
+ *
+ * @c: input character
+ *
+ * Return: Always zero
+ */
+
+int _isupper(int c);
+{
+int a, x = 0;
+
+for (a = 'A' ; a < 'Z' ; a++)
+{
+if (a == 'c')
+{
+x = x + 1;
+return (1);
+}
+}
+if (x == 0)
+return (0);
+}
