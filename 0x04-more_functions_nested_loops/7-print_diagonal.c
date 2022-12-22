5ddc94b50624aa348b90d743651277c0124e4678
Bereket Ferde <bereket547@example.com>
2022-12-22 09:01:53 -0800

    diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,7 +10,7 @@
 
 void print_diagonal(int n)
 {
-int a;
+int a, b;
 
 if (n == 0 || n < 0)
 _putchar('\n');
@@ -18,9 +18,12 @@ else
 {
 for (a = 0 ; a < n ; a++)
 {
+for (b = 0 ; b < a ; b++)
+{
 _putchar(' ');
 }
-_putchar('\\');
-}
+_putchar(92);
 _putchar('\n');
+}
+}
 }
