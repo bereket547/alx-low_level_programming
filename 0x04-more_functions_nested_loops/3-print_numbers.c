859f6b2601eb5590569047c0973bc2580ed676f9
Bereket Ferde <bereket547@example.com>
2022-12-22 08:02:31 -0800

    printing 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include"main.h"
+
+/**
+ * print_numbers - print 0-9
+ *
+ * Return: no return
+ */
+
+int print_numbers(void)
+{
+int a;
+
+for (a = 48 ; a <= 57 ; a++)
+_putchar(a + '0');
+_putchar('\n');
+}
