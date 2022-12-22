68a20f98063c9acddc47296b4e67cd99b6bfeb2a
Bereket Ferde <bereket547@example.com>
2022-12-22 08:11:57 -0800

    escape 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include"main.h"
+
+/**
+ * print_numbers - print 0-9
+ *
+ * Return: no return
+ */
+
+void print_numbers(void)
+{
+int a;
+
+for (a = 48 ; a <= 57 ; a++)
+{
+if (a != 50 || a != 52)
+_putchar(a);
+}
+_putchar('\n');
+}
