7dad8f4166217b9c15e8083136fb6d6894af2173
Bereket Ferde <bereket547@example.com>
2022-12-22 01:06:20 -0800

    new

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,7 +8,7 @@
  */
 void print_timestable(int n)
 {
-int a, b, c;
+int a, b, c, i = 0;
 if (n < 15 || n > 0)
 {
 for (a = 0 ; a <= n ; a++)
@@ -18,34 +18,22 @@ for (b = 0 ; b <= n ; b++)
 c = a * b;
 if (c < 10)
 {
-if (b == n)
-{
 _putchar(c + '0');
-break;
-}
-else
+if (b != n)
 {
-_putchar(c + '0');
 _putchar(',');
-_putchar(' ');
-_putchar(' ');
+for(i ; i < 3 ; i++)
 _putchar(' ');
 }
 }
 if (c >= 10 && c < 100)
 {
-if (b == n)
-{
 _putchar((c / 10) + '0');
 _putchar((c % 10) + '0');
-break;
-}
-else
+if (b != n)
 {
-_putchar((c / 10) + '0');
-_putchar((c % 10) + '0');
 _putchar(',');
-_putchar(' ');
+for (i ; i < 2 ; i++)
 _putchar(' ');
 }
 }
@@ -63,6 +51,5 @@ _putchar(' ');
 }
 _putchar('\n');
 }
-
 }
 }
