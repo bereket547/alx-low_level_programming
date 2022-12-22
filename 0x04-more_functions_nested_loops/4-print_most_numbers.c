913cbf417c685100dd439c6d526f659d17ba3dc2
Bereket Ferde <bereket547@example.com>
2022-12-22 08:14:40 -0800

    escape 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,12 +1,12 @@
 #include"main.h"
 
 /**
- * print_numbers - print 0-9
+ * print__most_numbers - print 0-9 except 2 and 4
  *
  * Return: no return
  */
 
-void print_numbers(void)
+void print_most_numbers(void)
 {
 int a;
 
