5878463f582ce3c907a5054d2b8889867b85d7a4
Bereket Ferde <bereket547@example.com>
2022-12-23 01:19:16 -0800

    pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,19 @@
+#include"main.h"
+
+/**
+ * reset_to_98 - pointer to change values
+ * @n: number
+ * Description: print a triangle
+ *
+ * Return: no return
+ */
+
+void reset_to_98(int *n)
+{
+int *n, a;
+
+a = 98;
+n = &a;
+*n = 98;
+return (0);
+}
