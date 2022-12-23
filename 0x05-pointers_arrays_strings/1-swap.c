9b403e8a2c0a093536279d22df2a4c70cc8beff3
Bereket Ferde <bereket547@example.com>
2022-12-23 01:50:38 -0800

    change value

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,32 +1,18 @@
 #include"main.h"
 
 /**
- * print_triangle - print a triangle
- * @size: size of square
- * Description: print a triangle
+ * swap_int - pointer to change values
+ * @a: first number
+ * @b: second number
+ * Description: print a number
  *
  * Return: no return
  */
 
-void print_triangle(int size)
+void swap_int(int *a, int *b)
 {
-int a, b, c;
-
-if (size <= 0)
-_putchar('\n');
-else
-{
-for (a = 0 ; a < size ; a++)
-{
-for (b = size - a ; b > 1 ; b--)
-{
-_putchar(' ');
-}
-for (c = 0 ; c <= a; c++)
-{
-_putchar(35);
-}
-_putchar('\n');
-}
-}
+int c;
+c = *a;
+*a = *b;
+*b = c;
 }
