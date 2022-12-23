fde85d187e434c9f6c4a54000cde0eaeb14f8d1a
Bereket Ferde <bereket547@example.com>
2022-12-22 23:56:43 -0800

    integers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -18,26 +18,10 @@ n = n * -1;
 k = n;
 _putchar('-');
 }
-if (k < 10 && k >= 0)
-_putchar(k + '0');
-if ((k >= 10) && (k <= 99))
-{
-_putchar((k / 10) + '0');
-_putchar((k % 10) + '0');
-}
-if ((k >= 100) && (k <= 999))
-{
-_putchar((k / 100) + '0');
-_putchar(((k % 100) / 10) + '0');
-_putchar(((k % 100) % 10) + '0');
-}  
-if ((k >= 1000) && (k <= 9999))
-{
-_putchar((k / 1000) + '0');
-_putchar(((k % 1000) / 100) + '0');
-_putchar((((k % 1000) % 100) / 10) + '0');
-_putchar((((k % 1000) % 100) % 10) + '0');
-}
+k /= 10;
+if (k != 0)
+print_number(k);
+_putchar((unsigned int) n % 10 + '0');
 _putchar('\n');
 return (0);
 }
