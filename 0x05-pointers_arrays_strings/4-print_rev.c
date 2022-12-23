eec57cfbdd2c535e2668793fc203cf1b83acba5a
Bereket Ferde <bereket547@example.com>
2022-12-23 02:44:31 -0800

    reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include"main.h"
+
+/**
+ * print_rev - print a reverse string
+ * @s: string value
+ * Return: no return
+ */
+
+void print_rev(char *s)
+{
+
+for (; *str != '\0' ; str--)
+{
+_putchar(*str);
+}
+_putchar('\n');
+}
+
